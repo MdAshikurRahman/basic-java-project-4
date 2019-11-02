f244fbfebf42b4bc59c307706439678bc7018663
MdAshikurRahman <mdashikurrahman.online@gmail.com>
2019-11-02 22:19:53 +0600

    new javaproject4.java file create

---
+++ JavaProject4.java
@@ -0,0 +1,27 @@
+
+package javaproject4;
+
+
+public class JavaProject4 {
+
+  
+    public static void main(String[] args) {
+       boolean b = true;
+       char c = 'a';
+       short s = 324;
+       int i = 478456865;
+       float f = 10.300f;
+       double d = 10.50000;
+       
+    
+       System.out.printf("boolean b = %b\n",b);
+       System.out.printf("Character c = %c\n",c);
+       System.out.printf("Short s = %d\n",s);
+       System.out.printf("integer i = %d\n",i);
+       System.out.printf("float f = %.1f\n",f);
+       System.out.printf("double d = %.2f\n",d);
+       
+  
+    }
+    
+}
